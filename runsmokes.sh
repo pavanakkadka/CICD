fae824997784c4482caebd9f5ce12cdd8aca056f
Adam Wezva <scmlearningcentre@gmail.com>
2021-07-16 13:04:34 +0000

    Update runsmokes.sh

--- runsmokes.sh
+++ runsmokes.sh
@@ -11,7 +11,7 @@ test_container()
 {
  log "INFO: Begin Docker Sanity check"
 
- TESTCASES=("HttpCheck", "LogCheck")
+ TESTCASES=("HttpCheck" "DBCheck")
  TURL=$1
  RETVAR=0
 
@@ -20,25 +20,16 @@ test_container()
    log "INFO: Running TestCase: $TC"
 
    if [ ${TC} == "HttpCheck" ]; then
-
      curl -is --max-redirs 10 http://$TURL -L | grep -w "200 OK"
-     if [ $? -ne "0" ]; then
-        log "ERROR: TESTCASE: $TC FAILED"
-        RETVAR=$((RETVAR + 1))
-     else
-        log "INFO: TESTCASE: $TC PASSED"
-     fi
-
    else
+     docker exec wezvadevdb redis-cli --version
+   fi
 
-     CWD=$(pwd)
-     if [ ! -f ${CWD}/sanity/server.log ]; then
-        log "ERROR: TESTCASE: $TC FAILED"
-        RETVAR=$((RETVAR + 1))
-     else
-        log "INFO: TESTCASE: $TC PASSED"
-     fi
-
+   if [ $? -ne "0" ]; then
+     log "ERROR: TESTCASE: $TC FAILED"
+     RETVAR=$((RETVAR + 1))
+   else
+     log "INFO: TESTCASE: $TC PASSED"
    fi
  done
 
@@ -46,23 +37,10 @@ test_container()
  return ${RETVAR}
 }
 
-cleanup()
-{
- log "INFO: Cleaning up Docker Container"
- CNAME=$1
- /usr/bin/docker-compose down
- if [ $? -ne "0" ]; then
-    log "ERROR: Couldnt delete Docker Container"
-    exit 1
- fi
- rm -rf sanity/
- log "INFO: Deleted Docker Container"
-}
 
 #MAIN#
 test_container ${TESTURL}
 TRET=$?
-cleanup testc
 if [ ${TRET} -ne "0" ]; then
   exit 1
 fi
